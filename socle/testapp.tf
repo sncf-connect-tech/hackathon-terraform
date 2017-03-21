43de6779bb0e8ec2de3710733f296f5f99696ae8
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-21 16:10:17 +0100

    remove pub ip

--- socle/testapp.tf
+++ socle/testapp.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "testapp" {
   ami                         = "ami-405f7226" 
   instance_type               = "m3.medium"
+  associate_public_ip_address = false
   subnet_id                   = "${aws_subnet.zone_appli.id}"
   vpc_security_group_ids      = ["${aws_security_group.allow_ssh_from_bastion.id}"]
   key_name                    = "${aws_key_pair.hackathon.key_name}"
