348bc5d08e6f918757cc5de6c6560abde77abcc0
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-21 13:41:32 +0100

    table routage

---
+++ socle/routes.tf
@@ -0,0 +1,13 @@
+resource "aws_route" "internet_route" {
+  "vpc_id = "${aws_vpc.mainVPC.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0""
+    gateway_id = "${aws_internet_gateway.gw-mainVPC.id}""
+  }
+
+  tags {
+    Name = "Routage public"
+    Owner = "Daniel Polombo"
+  }
+}
