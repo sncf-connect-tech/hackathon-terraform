4612a1e960f0b538011c74c0eda3e65c23d262f7
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-21 13:43:08 +0100

    update routage

--- socle/routes.tf
+++ socle/routes.tf
@@ -1,9 +1,9 @@
-resource "aws_route" "internet_route" {
-  "vpc_id = "${aws_vpc.mainVPC.id}"
+resource "aws_route_table" "internet_route" {
+  vpc_id = "${aws_vpc.mainVPC.id}"
 
   route {
-    cidr_block = "0.0.0.0/0""
-    gateway_id = "${aws_internet_gateway.gw-mainVPC.id}""
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw-mainVPC.id}"
   }
 
   tags {
