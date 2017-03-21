8a8889909f9fcfd78f4fa6982c027a88f95bcf3d
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-21 15:58:37 +0100

    test dns

--- socle/bastion.tf
+++ socle/bastion.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "bastion" {
   subnet_id                   = "${aws_subnet.zone_transverse.id}"
   vpc_security_group_ids      = ["${aws_security_group.allow_ssh_from_any.id}"]
   key_name                    = "${aws_key_pair.hackathon.key_name}"
+  public_dns                  = "bastion.${aws_route53_zone.myzone.name}"
 
   tags {
     Name  = "bastion"
