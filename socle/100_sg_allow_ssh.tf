59ede6777e369db5c84d3cbc526ccf809a1e9845
Lorenzati Yannick <YLorenzati.prestataire@voyages-sncf.com>
2017-03-21 11:18:09 +0100

    add ssh sg

---
+++ socle/100_sg_allow_ssh.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "allow_ssh" {
+  vpc_id      = ""
+  name_prefix = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.project_name} - ssh allow sg"
+  }
+}
