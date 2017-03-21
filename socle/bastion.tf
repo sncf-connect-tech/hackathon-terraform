3099623ed4a142f539f3cdb62d5f6d675076e631
nikko <nvindevogel.prestataire@voyages-sncf.com>
2017-03-21 15:34:01 +0100

    image changed

--- socle/bastion.tf
+++ socle/bastion.tf
@@ -1,20 +1,6 @@
-#data "aws_ami" "latest_amlinux" {
-	#most_recent = true
-
-    #filter {
-	#name = "virtualization-type"
-	#values = ["hvm"]
-   #}
-
-   #filter {
-	#name = "name"
-	#values = ["ADOPC-UBUNTU-16-*"]
-   #}
-#}
-
 resource "aws_instance" "bastion" {
 	ami = "ami-405f7226"
-	associate_public_ip_address = "1"
+	associate_public_ip_address = true
 	instance_type = "t2.micro"	
 	subnet_id = "${aws_subnet.zone_transverse.id}"
 	vpc_security_group_ids = ["${aws_security_group.allow_ssh_from_any.id}"]
