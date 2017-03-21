e6c90d1c94686e95e12416fadb3b950699552471
Lorenzati Yannick <YLorenzati.prestataire@voyages-sncf.com>
2017-03-21 10:32:12 +0100

    remove ssh key var for now

--- socle/terraform.tfvars
+++ socle/terraform.tfvars
@@ -1,6 +1,3 @@
 access_key = "AKIAIJRW6EDP4VUCVVWQ"
 secret_key = "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"
 
-ssh_key_name = "socle"
-ssh_public_key = ""
-
--- socle/variables.tf
+++ socle/variables.tf
@@ -3,11 +3,3 @@ variable "secret_key" {}
 variable "region" {
   default = "eu-west-1"
 }
-
-variable "ssh_key_name" {
-  default = "ssh/idrsa"
-}
-
-variable "ssh_public_key" {
-  default = "ssh/idrsa.pub"
-}
