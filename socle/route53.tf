59c5c9fe86651b2dfb6282fd3b692bd7fd219fac
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-21 16:00:42 +0100

    fix dns

--- socle/route53.tf
+++ socle/route53.tf
@@ -1,3 +1,12 @@
 data "aws_route53_zone" "myzone" {
   name = "vsct-hackathon.com"
 }
+
+resource "aws_route53_record" "bastion" {
+  zone_id = "${data.aws_route53_zone.myzone.zone_id}"
+  name = "bastion.${data.aws_route53_zone.myzone.name}"
+  type = "A"
+  ttl = "300"
+  records = ["${aws_instance.bastion.public_ip}"]
+}
+
