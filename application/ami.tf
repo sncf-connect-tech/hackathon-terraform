78394b7c0c789e85e0c1d83b1342ab4fbb04ab9f
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-22 13:38:51 +0100

    ami

--- application/ami.tf
+++ application/ami.tf
@@ -8,6 +8,6 @@ data "aws_ami" "latest_ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20170221"]
   }
 } 
