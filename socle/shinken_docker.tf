39ab6b38d4c9b55de5157c8aac5935a2f14de1a1
Monteyne Thomas <tmonteyne@voyages-sncf.com>
2017-03-22 10:00:27 +0100

    Ajout shinken

--- socle/shinken_docker.tf
+++ socle/shinken_docker.tf
@@ -2,8 +2,27 @@ data "template_file" "bootstrap_shinken" {
   template = "${file("files/bootstrap_shinken.sh")}"
 }
 
+data "aws_ami" "latest_amlinux" {
+  most_recent      = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["aws-elasticbeanstalk-amzn*docker*"]
+  }
+} 
+
 resource "aws_instance" "shinken" {
-  ami                         = "ami-405f7226" 
+  ami                         = "${data.aws_ami.latest_amlinux.id}" 
   associate_public_ip_address = true
   instance_type               = "m3.medium"
   subnet_id                   = "${aws_subnet.zone_transverse.id}"
