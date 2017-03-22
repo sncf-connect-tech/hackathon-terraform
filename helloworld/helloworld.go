40c9fddbf6045b66fea1f613d6a1a939a2866efb
Guillaume ARNAUD <garnaud25@gmail.com>
2017-03-22 10:35:25 +0100

    fix helloworld path

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hackathon 2017.\n\nnHello: %s!", r.URL.Path[1:])
+	hostname, _ := os.Hostname()
+	fmt.Fprintf(w, "Hackathon 2017.\n\nnHello: %s!\nhostname %s", r.URL.Path[1:], hostname)
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
