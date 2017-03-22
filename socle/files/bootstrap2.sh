7b66fb1e125de023c75bf50c5fa6a2b554f0fb8c
nikko <nvindevogel.prestataire@voyages-sncf.com>
2017-03-22 09:14:41 +0100

    creation

---
+++ socle/files/bootstrap2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+set -x
+
+# Variables
+LOGFILE=/var/log/user-data.log
+
+# Make sure we have a log for the bootstrap process
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec > >(tee -a $LOGFILE)
+exec 2>&1
+
+mkdir -p /home/admsys/.ssh
+useradd admsys
+cp -r /home/ubuntu/.ssh/* /home/admsys/.ssh/
+cp -r /home/ubuntu/.bashrc /home/admsys/
+cp -r /home/ubuntu/.profile /home/admsys/
+chmod 644 /home/admsys/.ssh/authorized_keys
+apt-get install ansible -y
+mv /etc/ansible /etc/ansible.orig
+apt-get install git -y 
+mkdir -p /appl/git/
+cd /appl/git/
+git clone https://github.com/voyages-sncf-technologies/hackathon-terraform.git
+ln -s /appl/git/hackathon-terraform/application/ansible /etc/ansible
