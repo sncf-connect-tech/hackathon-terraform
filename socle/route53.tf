2deda4d3907945b95fda87869cfe5c1b8192c150
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-22 14:09:21 +0100

    suppression bastion2

--- socle/route53.tf
+++ socle/route53.tf
@@ -18,14 +18,6 @@ resource "aws_route53_record" "bastion" {
   records = ["${aws_instance.bastion.public_ip}"]
 }
 
-resource "aws_route53_record" "bastion2" {
-  zone_id = "${data.aws_route53_zone.myzone.zone_id}"
-  name    = "bastion2.${data.aws_route53_zone.myzone.name}"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.bastion2.public_ip}"]
-}
-
 resource "aws_route53_record" "lb-bastion1" {
   zone_id = "${data.aws_route53_zone.myzone.zone_id}"
   name    = "lb-bastion.${data.aws_route53_zone.myzone.name}"
@@ -38,18 +30,4 @@ resource "aws_route53_record" "lb-bastion1" {
 
   set_identifier = "bastion"
   records        = ["bastion.${data.aws_route53_zone.myzone.name}"]
-}
-
-resource "aws_route53_record" "lb-bastion2" {
-  zone_id = "${data.aws_route53_zone.myzone.zone_id}"
-  name    = "lb-bastion.${data.aws_route53_zone.myzone.name}"
-  type    = "CNAME"
-  ttl     = "1"
-
-  weighted_routing_policy {
-    weight = 33
-  }
-
-  set_identifier = "bastion2"
-  records        = ["bastion2.${data.aws_route53_zone.myzone.name}"]
 }
