cec93eb2ee016deac22c418368e378be28bfa457
Daniel Polombo <dpolombo@voyages-sncf.com>
2017-03-22 13:56:49 +0100

    ami appli

--- application/createInstance.tf
+++ application/createInstance.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "apache" {
-  ami = "${data.aws_ami.latest_ubuntu.id}"
+#  ami = "${data.aws_ami.latest_ubuntu.id}"
+  ami = "ami-f4cc1de2"
   instance_type = "m3.medium"
   key_name = "${data.terraform_remote_state.socle.socle_key_name}"
   count = 2
