397778e32a37ddd07980c2f01e07f6c09ebf9472
Monteyne Thomas <tmonteyne@voyages-sncf.com>
2017-03-22 09:49:48 +0100

    Ajout shinken

--- socle/route53.tf
+++ socle/route53.tf
@@ -2,6 +2,14 @@ data "aws_route53_zone" "myzone" {
   name = "vsct-hackathon.com"
 }
 
+resource "aws_route53_record" "shinken" {
+  zone_id = "${data.aws_route53_zone.myzone.zone_id}"
+  name = "shinken.${data.aws_route53_zone.myzone.name}"
+  type = "A"
+  ttl = "300"
+  records = ["${aws_instance.shinken.public_ip}"]
+}
+
 resource "aws_route53_record" "bastion" {
   zone_id = "${data.aws_route53_zone.myzone.zone_id}"
   name = "bastion.${data.aws_route53_zone.myzone.name}"
