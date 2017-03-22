a719b3761f78a9966d3b58cd3be4a0c224376627
Auffredou Thomas <tauffredou.prestataire@voyages-sncf.com>
2017-03-22 12:19:58 +0100

    Update default route for sgbd and appli

--- socle/routes.tf
+++ socle/routes.tf
@@ -21,3 +21,28 @@ resource "aws_route_table_association" "routing_zone_transverse" {
   route_table_id = "${aws_route_table.internet_route.id}"
   subnet_id      = "${aws_subnet.zone_transverse.id}"
 }
+
+
+/* Routing table for private subnet */
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.mainVPC.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_nat_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "${var.project_name} - private route table"
+  }
+}
+
+resource "aws_route_table_association" "routing_zone_appli" {
+  route_table_id = "${aws_route_table.private.id}"
+  subnet_id      = "${aws_subnet.zone_appli.id}"
+}
+
+resource "aws_route_table_association" "routing_zone_sgbd" {
+  route_table_id = "${aws_route_table.private.id}"
+  subnet_id      = "${aws_subnet.zone_sgbd.id}"
+}
