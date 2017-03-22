360df3bb1f15142c391403340784726f0ea50a3e
Lorenzati Yannick <YLorenzati.prestataire@voyages-sncf.com>
2017-03-22 14:39:44 +0100

    add elb

--- application/createInstance.tf
+++ application/createInstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "apache" {
-  ami = "ami-f9e151ef"
+  ami = "${data.aws_ami.latest_ubuntu.id}"
 
   #ami           = "ami-f4cc1de2"
   instance_type     = "m3.medium"
@@ -20,7 +20,7 @@ resource "aws_instance" "apache" {
 }
 
 resource "aws_instance" "tomcat" {
-  ami = "${data.aws_ami.latest_ubuntu.id}"
+  ami = "ami-f9e151ef"
 
   #ami           = "ami-f4cc1de2"
   instance_type     = "m3.medium"
@@ -38,6 +38,39 @@ resource "aws_instance" "tomcat" {
     "${data.terraform_remote_state.socle.socle_security_group_allow_ssh_from_bastion_id}",
     "${data.terraform_remote_state.socle.socle_security_group_sg_app_id}",
   ]
+}
+
+
+resource "aws_elb" "hackathon" {
+
+  # refactor this
+  subnets = ["${data.terraform_remote_state.socle.socle_subnet_sas_securite_id}"]
+  security_groups = [
+    "${data.terraform_remote_state.socle.socle_security_group_allow_ssh_from_bastion_id}",
+    "${data.terraform_remote_state.socle.socle_security_group_sg_web_id}",
+  ]
+
+  listener {
+    instance_port = 8080
+    instance_protocol = "tcp"
+    lb_port = 80
+    lb_protocol = "tcp"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 5
+    target = "TCP:8080"
+    interval = 5
+    timeout = 4
+  }
+
+  instances = ["${aws_instance.tomcat.*.id}"]
+
+  tags = {
+    Name  = "elb"
+    Owner = "prevellin"
+  }
 }
 
 output "ip_apache" {
